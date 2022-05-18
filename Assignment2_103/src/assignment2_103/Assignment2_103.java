b30dfffd07d53edcf3a2ae4622ad8ebb9758d697
MaxwellHinton <qwv9850@autuni.ac.nz>
2022-05-18 16:29:16 +1200

    first check commit

---
+++ Assignment2_103/src/assignment2_103/Assignment2_103.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2_103;
+
+/**
+ *
+ * @author maxhi
+ */
+public class Assignment2_103 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
